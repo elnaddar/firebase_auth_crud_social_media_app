b2dbaa54070b40b189cc15dc54d232fb06ab8cbd
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-15 12:05:15 +0300

    login page requires login form.

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth_crud_social_media_app/views/login_form/login_form.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,6 +6,34 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.person,
+                  size: 80,
+                  color: theme.colorScheme.inversePrimary,
+                ),
+                const Text(
+                  "SOCIAL",
+                  style: TextStyle(
+                    fontSize: 20,
+                    letterSpacing: 6,
+                  ),
+                ),
+                const SizedBox(height: 25),
+                const LoginForm()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
