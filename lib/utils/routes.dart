ef7ab5d372c46975dab03369d304b4d4f6f7d380
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-15 09:57:07 +0300

    moved routes to utils/src and created routes manager.

--- lib/utils/routes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:firebase_auth_crud_social_media_app/views/login_page.dart';
-import 'package:flutter/material.dart';
-
-final Map<String, LoginPage Function(BuildContext)> routes = {
-  '/login': (BuildContext context) => const LoginPage(),
-};
