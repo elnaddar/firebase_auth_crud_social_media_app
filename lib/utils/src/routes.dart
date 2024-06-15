a038d8f72ed2a3acdf9f11462f7d07470bc6c48e
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-15 09:59:56 +0300

    updated routes manager to have initial route

--- lib/utils/src/routes.dart
+++ lib/utils/src/routes.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth_crud_social_media_app/views/login_page.dart';
 import 'package:flutter/material.dart';
 
 class RoutesManager {
+  final String initialRoute = "/login";
   final Map<String, LoginPage Function(BuildContext)> routes = {
     '/login': (BuildContext context) => const LoginPage(),
   };
