5b64a3452bc5f6f019ef54a920f2efb7f47e79ad
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-15 09:57:35 +0300

    created utils manager.

---
+++ lib/utils/utils_manager.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_auth_crud_social_media_app/utils/src/routes.dart';
+import 'package:firebase_auth_crud_social_media_app/utils/src/theme.dart';
+
+class UtilsManager {
+  final themeManager = ThemeManager();
+  final routesManager = RoutesManager();
+}
