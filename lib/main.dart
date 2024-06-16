c42156f993ea569422c43725fb91bd7e3e393990
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-16 11:35:18 +0300

    initialized firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_auth_crud_social_media_app/firebase_options.dart';
 import 'package:firebase_auth_crud_social_media_app/utils/utils_manager.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MainApp());
 }
 
