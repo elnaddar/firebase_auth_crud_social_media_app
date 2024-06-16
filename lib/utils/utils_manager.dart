0fe51dedd742bf9e26c71cc36ea8c8a7dd208467
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-16 16:07:29 +0300

    updated utils manager

--- lib/utils/utils_manager.dart
+++ lib/utils/utils_manager.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_auth_crud_social_media_app/auth/auth_cubit.dart';
 import 'package:firebase_auth_crud_social_media_app/utils/src/routes.dart';
 import 'package:firebase_auth_crud_social_media_app/utils/src/theme.dart';
 
 class UtilsManager {
+  UtilsManager({required this.authCubit})
+      : routesManager = RoutesManager(authCubit: authCubit);
+  final AuthCubit authCubit;
+  final RoutesManager routesManager;
   final themeManager = ThemeManager();
-  final routesManager = RoutesManager();
 }
