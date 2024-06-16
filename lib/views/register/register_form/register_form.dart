f251f6306c2e78f11b693dab9e130fab7b3584d0
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-16 12:53:39 +0300

    added validation logic to register form

--- lib/views/register/register_form/register_form.dart
+++ lib/views/register/register_form/register_form.dart
@@ -34,6 +34,15 @@ class _RegisterFormState extends State<RegisterForm> {
               hintText: "Email",
               textInputAction: TextInputAction.next,
               keyboardType: TextInputType.emailAddress,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter an email address';
+                } else if (!RegExp(r'^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$')
+                    .hasMatch(value)) {
+                  return 'Please enter a valid email address';
+                }
+                return null; // Return null if the input is valid
+              },
             ),
             const SizedBox(height: 10),
             InputFormField(
@@ -43,6 +52,36 @@ class _RegisterFormState extends State<RegisterForm> {
               obscureText: true,
               textInputAction: TextInputAction.next,
               keyboardType: TextInputType.visiblePassword,
+              validator: (password) {
+                if (password == null || password.trim().isEmpty) {
+                  return "Password field is required.";
+                }
+                String errorMessage = '';
+                // Password length greater than 6
+                if (password.length < 6) {
+                  errorMessage +=
+                      '• Password must be longer than 6 characters.\n';
+                }
+                // Contains at least one uppercase letter
+                if (!password.contains(RegExp(r'[A-Z]'))) {
+                  errorMessage += '• Uppercase letter is missing.\n';
+                }
+                // Contains at least one lowercase letter
+                if (!password.contains(RegExp(r'[a-z]'))) {
+                  errorMessage += '• Lowercase letter is missing.\n';
+                }
+                // Contains at least one digit
+                if (!password.contains(RegExp(r'[0-9]'))) {
+                  errorMessage += '• Digit is missing.\n';
+                }
+                // Contains at least one special character
+                if (!password.contains(RegExp(r'[!@#%^&*(),.?":{}|<>]'))) {
+                  errorMessage += '• Special character is missing.\n';
+                }
+                // If there are no error messages, the password is valid
+                if (errorMessage.isEmpty) return null;
+                return errorMessage;
+              },
             ),
             const SizedBox(height: 10),
             InputFormField(
@@ -52,6 +91,13 @@ class _RegisterFormState extends State<RegisterForm> {
               obscureText: true,
               textInputAction: TextInputAction.done,
               keyboardType: TextInputType.visiblePassword,
+              validator: (value) {
+                if (formMap[RegisterFormEnum.confirmPassword] !=
+                    formMap[RegisterFormEnum.password]) {
+                  return "Passwords must match";
+                }
+                return null;
+              },
             ),
             const SizedBox(height: 10),
             Align(
@@ -68,7 +114,7 @@ class _RegisterFormState extends State<RegisterForm> {
             const SizedBox(height: 32),
             FormButton(
               text: "Register",
-              onTap: () {},
+              onTap: _submitRegister,
             ),
             const SizedBox(height: 10),
             Text.rich(
@@ -92,5 +138,9 @@ class _RegisterFormState extends State<RegisterForm> {
             )
           ],
         ));
+  }
+
+  void _submitRegister() {
+    formMap.submit((data) {}, saveBeforeValidate: true);
   }
 }
