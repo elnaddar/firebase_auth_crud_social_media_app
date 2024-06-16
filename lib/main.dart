7feb31d1a5c5d6eb4d3efec65506e7a38577acf7
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-16 12:56:08 +0300

    added firebase auth service provider to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:firebase_auth_crud_social_media_app/firebase_options.dart';
+import 'package:firebase_auth_crud_social_media_app/services/firebase_auth_service.dart';
 import 'package:firebase_auth_crud_social_media_app/utils/utils_manager.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MainApp());
+  runApp(RepositoryProvider(
+    create: (context) => FirebaseAuthService(),
+    child: MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
