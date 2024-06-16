9830093052dd0269d542e351a1c56ecc155697d5
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-16 16:11:26 +0300

    updated main to handle authentication changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth_crud_social_media_app/auth/auth_cubit.dart';
 import 'package:firebase_auth_crud_social_media_app/firebase_options.dart';
 import 'package:firebase_auth_crud_social_media_app/services/firebase_auth_service.dart';
 import 'package:firebase_auth_crud_social_media_app/utils/utils_manager.dart';
@@ -10,23 +11,27 @@ Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(RepositoryProvider(
     create: (context) => FirebaseAuthService(),
-    child: MainApp(),
+    child: BlocProvider(
+      create: (context) => AuthCubit(context.read<FirebaseAuthService>()),
+      child: const MainApp(),
+    ),
   ));
 }
 
 class MainApp extends StatelessWidget {
-  final UtilsManager utils = UtilsManager();
-
-  MainApp({super.key});
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final authCubit = BlocProvider.of<AuthCubit>(context, listen: true);
+    final UtilsManager utils = UtilsManager(authCubit: authCubit);
+
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      initialRoute: utils.routesManager.initialRoute,
-      routes: utils.routesManager.routes,
       theme: utils.themeManager.lightMode,
       darkTheme: utils.themeManager.darkMode,
+      routeInformationParser: utils.routesManager.routeInformationParser,
+      routerDelegate: utils.routesManager.routerDelegate,
       themeMode: ThemeMode.system,
     );
   }
