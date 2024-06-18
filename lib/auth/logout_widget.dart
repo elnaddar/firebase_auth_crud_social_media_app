1e760be73bd91f168898ad97c9803d6aa7f0bd12
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-18 11:39:44 +0300

    created widget to handle logout

---
+++ lib/auth/logout_widget.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_crud_social_media_app/services/firebase_auth_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LogoutWidget extends StatelessWidget {
+  const LogoutWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    RepositoryProvider.of<FirebaseAuthService>(context)
+        .signOut()
+        .onError((FirebaseAuthException error, stackTrace) {
+      if (context.mounted) {
+        Navigator.pop(context);
+        showAdaptiveDialog(
+          context: context,
+          builder: (context) => AlertDialog.adaptive(
+            title: const Text("Error"),
+            content: Text(error.code),
+          ),
+        );
+      }
+    });
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator.adaptive(),
+      ),
+    );
+  }
+}
