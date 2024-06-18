7e9add8b0e3739016cacf011e9bf000a6cb72d5e
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-18 16:10:01 +0300

    users page shows all users

--- lib/views/users/users_page.dart
+++ lib/views/users/users_page.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth_crud_social_media_app/components/app_drawer.dart';
+import 'package:firebase_auth_crud_social_media_app/repository/users_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UsersPage extends StatelessWidget {
   const UsersPage({super.key});
@@ -10,6 +12,32 @@ class UsersPage extends StatelessWidget {
       drawer: const AppDrawer(selectedIndex: 2),
       appBar: AppBar(
         title: const Text("Users"),
+      ),
+      body: StreamBuilder(
+        stream:
+            RepositoryProvider.of<UsersRepository>(context).getUsersSnapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Something went wrong try again."),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator.adaptive());
+          }
+
+          final users = snapshot.data!.docs;
+          return ListView.separated(
+              itemBuilder: (context, index) {
+                final user = users[index].data();
+                return ListTile(
+                  title: Text(user['name']),
+                  subtitle: Text(user['email']),
+                );
+              },
+              separatorBuilder: (context, index) => const Divider(),
+              itemCount: users.length);
+        },
       ),
     );
   }
