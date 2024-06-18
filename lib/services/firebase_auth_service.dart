3e5b2ab2e9ad941bd6e9c09fe4b2fc07541a2c12
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-18 14:15:09 +0300

    firebase auth now accepts username

--- lib/services/firebase_auth_service.dart
+++ lib/services/firebase_auth_service.dart
@@ -2,10 +2,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthService {
   final _instance = FirebaseAuth.instance;
-  Future<UserCredential> createUserWithEmailAndPassword(
-          {required String email, required String password}) =>
-      _instance.createUserWithEmailAndPassword(
-          email: email, password: password);
+  Future<UserCredential> createUserWithEmailAndPassword({
+    String? username,
+    required String email,
+    required String password,
+  }) {
+    return _instance
+        .createUserWithEmailAndPassword(email: email, password: password)
+        .then((cred) async {
+      await cred.user!.updateDisplayName(username);
+      return cred;
+    });
+  }
 
   Future<UserCredential> signInWithEmailAndPassword(
           {required String email, required String password}) =>
@@ -14,4 +22,11 @@ class FirebaseAuthService {
   Stream<User?> stateChanges() => _instance.authStateChanges();
   Future<void> signOut() => _instance.signOut();
   User? get currentUser => _instance.currentUser;
+  bool get isVerified {
+    if (currentUser != null) {
+      return currentUser!.emailVerified;
+    } else {
+      return false;
+    }
+  }
 }
