c653cabf3f7763c5e46273b7c3f003cefb40c4e5
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-21 21:04:04 +0300

    updated home page

--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth_crud_social_media_app/components/app_drawer.dart';
 import 'package:firebase_auth_crud_social_media_app/repository/posts_repository.dart';
 import 'package:firebase_auth_crud_social_media_app/repository/users_repository.dart';
+import 'package:firebase_auth_crud_social_media_app/views/home/posts/posts_builder.dart';
 import 'package:firebase_auth_crud_social_media_app/views/home/posts/write_post_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,11 +13,10 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final usersRepo = RepositoryProvider.of<UsersRepository>(context);
-    final posts = PostsRepository(
-        usersRepo: usersRepo, firestoreService: usersRepo.storeService);
 
-    return RepositoryProvider.value(
-      value: posts,
+    return RepositoryProvider(
+      create: (BuildContext context) => PostsRepository(
+          usersRepo: usersRepo, firestoreService: usersRepo.storeService),
       child: Scaffold(
         drawer: const AppDrawer(),
         appBar: AppBar(
@@ -28,39 +28,11 @@ class HomePage extends StatelessWidget {
                 icon: const Icon(Icons.logout))
           ],
         ),
-        body: CustomScrollView(slivers: [
-          const SliverToBoxAdapter(child: WritePostWidget()),
-          StreamBuilder(
-            stream: posts
-                .getPosts()
-                .orderBy('timestamp', descending: true)
-                .snapshots(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return const SliverToBoxAdapter(
-                  child: Center(
-                    child: Text("Something went wrong try again."),
-                  ),
-                );
-              }
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const SliverToBoxAdapter(
-                    child: Center(child: CircularProgressIndicator.adaptive()));
-              }
-              if (snapshot.data == null || snapshot.data!.docs.isEmpty) {
-                return const SliverToBoxAdapter(
-                    child: Center(child: Text("No posts till now.")));
-              }
-              final items = snapshot.data!.docs;
-              return SliverList.builder(
-                itemBuilder: (context, index) {
-                  final data = items[index].data();
-                  return ListTile(title: Text(data['content']));
-                },
-                itemCount: items.length,
-              );
-            },
-          )
+        body: const CustomScrollView(slivers: [
+          SliverToBoxAdapter(child: WritePostWidget()),
+          SliverToBoxAdapter(child: SizedBox(height: 6)),
+          PostsBuilder(),
+          SliverToBoxAdapter(child: SizedBox(height: 12)),
         ]),
       ),
     );
