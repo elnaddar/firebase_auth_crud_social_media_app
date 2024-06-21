fc611b58b2a0f9f05c16c6ffb8f5aa1fc9d35452
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-21 19:31:16 +0300

    write post can now send data to firestore.

--- lib/views/home/posts/write_post_widget.dart
+++ lib/views/home/posts/write_post_widget.dart
@@ -1,7 +1,28 @@
+import 'package:firebase_auth_crud_social_media_app/repository/posts_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:form_map/form_map.dart';
 
-class WritePostWidget extends StatelessWidget {
+enum PostEnum { title, content, imageURL }
+
+class WritePostWidget extends StatefulWidget {
   const WritePostWidget({super.key});
+
+  @override
+  State<WritePostWidget> createState() => _WritePostWidgetState();
+}
+
+class _WritePostWidgetState extends State<WritePostWidget> {
+  final formMap = FormMap<PostEnum>(key: GlobalKey<FormState>());
+
+  _submit() {
+    formMap.submit((data) {
+      context
+          .read<PostsRepository>()
+          .createPost(content: formMap[PostEnum.content]);
+    });
+    formMap.key.currentState!.reset();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,31 +33,41 @@ class WritePostWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(8),
         border: Border.all(color: Colors.black),
       ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const TextField(
-            minLines: 3,
-            maxLines: 5,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              focusedBorder: InputBorder.none,
-              fillColor: Colors.transparent,
-              hintText: "What do you think...",
+      child: Form(
+        key: formMap.key,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              minLines: 3,
+              maxLines: 5,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                fillColor: Colors.transparent,
+                hintText: "What do you think...",
+              ),
+              onSaved: (newValue) => formMap[PostEnum.content] = newValue,
+              validator: (value) {
+                if (value == null || value.trim().length < 3) {
+                  return "Post content can't be less than 3 characters.";
+                }
+                return null;
+              },
             ),
-          ),
-          SizedBox(
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(
-                  side: BorderSide.none,
-                  shape: const RoundedRectangleBorder(),
-                  foregroundColor: Colors.deepPurple,
-                ),
-                child: const Text("Post"),
-              ))
-        ],
+            SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: _submit,
+                  style: TextButton.styleFrom(
+                    side: BorderSide.none,
+                    shape: const RoundedRectangleBorder(),
+                    foregroundColor: Colors.deepPurple,
+                  ),
+                  child: const Text("Post"),
+                ))
+          ],
+        ),
       ),
     );
   }
