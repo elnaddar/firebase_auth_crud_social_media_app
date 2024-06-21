e50c56f4f32da7d0b18f4a59900d6ad056132f0f
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-06-21 21:02:57 +0300

    created function to format timestamp

---
+++ lib/helpers/format_timestamp.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+String formatTimestamp(Timestamp timestamp) {
+  DateTime date = timestamp.toDate();
+  DateTime now = DateTime.now();
+  Duration difference = now.difference(date);
+
+  if (difference.inSeconds < 60) {
+    return 'just now';
+  } else if (difference.inMinutes < 60) {
+    return '${difference.inMinutes} minute${difference.inMinutes > 1 ? 's' : ''} ago';
+  } else if (difference.inHours < 24) {
+    return '${difference.inHours} hour${difference.inHours > 1 ? 's' : ''} ago';
+  } else if (difference.inDays < 7) {
+    return '${difference.inDays} day${difference.inDays > 1 ? 's' : ''} ago';
+  } else if (difference.inDays < 30) {
+    return '${(difference.inDays / 7).floor()} week${(difference.inDays / 7).floor() > 1 ? 's' : ''} ago';
+  } else if (difference.inDays < 365) {
+    return '${(difference.inDays / 30).floor()} month${(difference.inDays / 30).floor() > 1 ? 's' : ''} ago';
+  } else {
+    return '${(difference.inDays / 365).floor()} year${(difference.inDays / 365).floor() > 1 ? 's' : ''} ago';
+  }
+}
